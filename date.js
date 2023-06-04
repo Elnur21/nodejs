be352209aaedb1d2b4ce7ed7839e8fbefc3f8ae6
Elnur21 <elnurme@code.edu.az>
2023-06-04 04:50:23 +0400

    colofrul print

--- date.js
+++ date.js
@@ -1,7 +1,12 @@
 let d2 = new Date(2023, 06, 1);
 
+function colorfulPrint(print, string) {
+  print(string);
+}
+
 setInterval(() => {
   let d1 = new Date();
+  let random = Math.floor(Math.random() * 10);
   let difference = d2 - d1;
   let days = Math.floor(difference / 1000 / 60 / 60 / 24);
   let hours = Math.floor((difference / 1000 / 60 / 60 / 24 - days) * 24);
@@ -18,15 +23,18 @@ setInterval(() => {
       seconds) *
       1000
   );
-  console.warn(
-    `${days > 9 ? days : `0${days}`} days, ${
-      hours > 9 ? hours : `0${hours}`
-    }:${minutes > 9 ? minutes : `0${minutes}`}:${
-      seconds > 9 ? seconds : `0${seconds}`
-    }:${milli_seconds > 9 ? milli_seconds : `0${milli_seconds}`}`
+  let print =
+    random < 5 ? console.warn : random < 7 ? console.error : console.log;
+  colorfulPrint(
+    print,
+    `${days > 9 ? days : `0${days}`} days, ${hours > 9 ? hours : `0${hours}`}:${
+      minutes > 9 ? minutes : `0${minutes}`
+    }:${seconds > 9 ? seconds : `0${seconds}`}:${
+      milli_seconds > 9 ? milli_seconds : `0${milli_seconds}`
+    }`
   );
+
   setTimeout(() => {
     console.clear();
   }, 500);
 }, 1000);
-
