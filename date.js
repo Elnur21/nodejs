d42027cda80bc7f91c3f9096a1558f268fb43d37
Elnur21 <elnurme@code.edu.az>
2023-06-04 04:34:45 +0400

    after setInterval

--- date.js
+++ date.js
@@ -1,17 +1,32 @@
-let d1 = new Date();
 let d2 = new Date(2023, 06, 1);
-let difference = d2-d1
-let days = Math.floor(difference/1000/60/60/24)
-let hours = Math.floor((difference/1000/60/60/24 - days)*24)
-let minutes = Math.floor(((difference/1000/60/60/24 -days)*24-hours)*60)
-let seconds = Math.floor((((difference/1000/60/60/24 -days)*24-hours)*60-minutes)*60)
-let milli_seconds = Math.floor(((((difference/1000/60/60/24 -days)*24-hours)*60-minutes)*60-seconds)*1000)
 
+setInterval(() => {
+  let d1 = new Date();
+  let difference = d2 - d1;
+  let days = Math.floor(difference / 1000 / 60 / 60 / 24);
+  let hours = Math.floor((difference / 1000 / 60 / 60 / 24 - days) * 24);
+  let minutes = Math.floor(
+    ((difference / 1000 / 60 / 60 / 24 - days) * 24 - hours) * 60
+  );
+  let seconds = Math.floor(
+    (((difference / 1000 / 60 / 60 / 24 - days) * 24 - hours) * 60 - minutes) *
+      60
+  );
+  let milli_seconds = Math.floor(
+    ((((difference / 1000 / 60 / 60 / 24 - days) * 24 - hours) * 60 - minutes) *
+      60 -
+      seconds) *
+      1000
+  );
+  console.warn(
+    `${days > 9 ? days : `0${days}`} days, ${
+      hours > 9 ? hours : `0${hours}`
+    }:${minutes > 9 ? minutes : `0${minutes}`}:${
+      seconds > 9 ? seconds : `0${seconds}`
+    }:${milli_seconds > 9 ? milli_seconds : `0${milli_seconds}`}`
+  );
+  setTimeout(() => {
+    console.clear();
+  }, 500);
+}, 1000);
 
-console.log(d1);
-console.log(d2);
-console.log(days)
-console.log(hours)
-console.log(minutes)
-console.log(seconds)
-console.log(milli_seconds)
